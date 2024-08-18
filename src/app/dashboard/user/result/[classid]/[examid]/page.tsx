1f3fe346b2f487d341a1c233f622243a6020722f
_userNull <ramzprivatexd@gmail.com>
2024-08-18 11:22:36 +0700

    fix: overflow text inside question box in result page

--- src/app/dashboard/user/result/[classid]/[examid]/page.tsx
+++ src/app/dashboard/user/result/[classid]/[examid]/page.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import { Exam, Questions } from "@/app/api/[[...route]]/types/exam";
 import QuestionsDisplay from "@/components/questions";
 import Splash from "@/components/splash";
@@ -7,37 +7,37 @@ import { QuestionsContext } from "@/context/Questions";
 import { useParams } from "next/navigation";
 import { useEffect, useState } from "react";
 
-export default function ExamView(){
-    const { examid , classid } = useParams()
-    let [load, setLoad] = useState(true)
-    let [result, setResult] = useState({} as any)
+export default function ExamView() {
+    const { examid, classid } = useParams();
+    let [load, setLoad] = useState(true);
+    let [result, setResult] = useState({} as any);
 
     useEffect(() => {
         fetch("/api/auth/")
             .then((r) => r.json())
             .then((json) => {
-                if (json.status != "OK") document.location.href = "/signin"
+                if (json.status != "OK") document.location.href = "/signin";
                 switch (json.data.role) {
                     case "user":
-                        setLoad(!load)
-                        break
+                        setLoad(!load);
+                        break;
                     case "instructor":
-                        document.location.href = "/dashboard/instructor"
-                        break
+                        document.location.href = "/dashboard/instructor";
+                        break;
                     case "admin":
-                        document.location.href = "/dashboard/admin"
-                        break
+                        document.location.href = "/dashboard/admin";
+                        break;
                 }
-            })
-    }, [])
+            });
+    }, []);
 
     useEffect(() => {
-        fetch("/api/user/class/"+classid+"/"+examid+"/result")
+        fetch("/api/user/class/" + classid + "/" + examid + "/result")
             .then((r) => r.json())
             .then((json) => {
-                setResult(json.data)
-            })
-    }, [load])
+                setResult(json.data);
+            });
+    }, [load]);
 
     return (
         <>
@@ -55,15 +55,19 @@ export default function ExamView(){
                     </div>
                     <h4 className="text-2xl font-bold py-4 px-5 text-center text-gray-600">Detail</h4>
                     <div className="h-[12rem] border grid grid-cols-1 overflow-y-auto">
-                        {result?.detail?.map((v: {question: string, correct: boolean}, i: number) => (
-                            <div className={"border flex flex-col justify-center px-3 min-h-[4rem] "+ (v.correct ? "bg-green-300" : "bg-red-300")}>
-                                <h5 className="">{i+1}. {v.question}</h5>
+                        {result?.detail?.map((v: { question: string; correct: boolean }, i: number) => (
+                            <div className={"border flex flex-col justify-center px-3 py-2 " + (v.correct ? "bg-green-300" : "bg-red-300")}>
+                                <h5 className="">
+                                    {i + 1}. {v.question}
+                                </h5>
                             </div>
                         ))}
                     </div>
-                    <a href={"/dashboard/user/class/"+classid} className="bg-[#ff7854] hover:bg-[#ff4c1a] my-5 px-5 py-3 text-lg font-semibold text-gray-100 rounded-3xl mx-3 flex items-center justify-center">Back</a>
+                    <a href={"/dashboard/user/class/" + classid} className="bg-[#ff7854] hover:bg-[#ff4c1a] my-5 px-5 py-3 text-lg font-semibold text-gray-100 rounded-3xl mx-3 flex items-center justify-center">
+                        Back
+                    </a>
                 </div>
             </div>
         </>
-    )
+    );
 }
