b9a026047961f1d75edb32c318fa77bade33e9d6
BoboiAzumi <untukherokudeploy@gmail.com>
2024-08-18 09:08:35 +0700

    Security Update

--- src/app/api/[[...route]]/auth/middleware.ts
+++ src/app/api/[[...route]]/auth/middleware.ts
@@ -1,5 +1,8 @@
 import { Context } from "hono"
-import { getCookie } from "hono/cookie"
+import { getCookie, setCookie } from "hono/cookie"
+import { verify } from "./jwtauth";
+import { findUserById } from "../db/users";
+import { Users } from "../types/user";
 
 export async function authentication(c: Context, next: Function){
     const jwt = getCookie(c, "jwt");
@@ -9,6 +12,36 @@ export async function authentication(c: Context, next: Function){
             status: "NOT_LOGGED"
         });
     }
-    await next()
+
+    const jwtver = await verify(jwt)
+
+    try{
+        const user: Users[] = await findUserById(jwtver.result ? jwtver.result._id : "");
+
+        if(user.length == 0){
+            c.status(500);
+            setCookie(c, "jwt", "")
+            return c.json({
+                status: "NOT_LOGGED"
+            });
+        }
+    
+        if(user[0].role != jwtver.result.role){
+            c.status(500);
+            setCookie(c, "jwt", "")
+            return c.json({
+                status: "NOT_LOGGED"
+            });
+        }
+    
+        await next()
+    }
+    catch{
+        c.status(500);
+        setCookie(c, "jwt", "")
+        return c.json({
+            status: "NOT_LOGGED"
+        });
+    }
 }
 
